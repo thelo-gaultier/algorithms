b43735e507d428b0cd68855f53ecdd20a43c0c62
Thelo <thelo.gaultier@gmail.com>
2016-04-19 23:19:51 +0800

    Clean node.

--- algorithms/datastructures/node.py
+++ algorithms/datastructures/node.py
@@ -4,14 +4,14 @@ Created on Apr 12, 2016
 @author: Thelo
 '''
 
+
 class Node:
 
-   def __init__(self, key):
-       self.key = key
-       self.pointers = {}
-       
-   def equals(self, another_node):
-      return (
-              another_node != None and 
-              self.key == another_node.key
-             )
+    def __init__(self, key):
+        self.key = key
+        self.pointers = {}
+
+    def equals(self, another_node):
+        return (
+            another_node is not None and
+            self.key == another_node.key)
